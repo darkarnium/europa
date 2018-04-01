c66d8ab3bfa9ddd201ee5c3bf3f89551007d4943
Peter Adkins <peter.adkins@kernelpicnic.net>
2018-04-01 16:13:06 +0100

    Clean-up.

--- src/europa/poller/poller.py
+++ src/europa/poller/poller.py
@@ -11,9 +11,6 @@ import RPi.GPIO as GPIO
 import Adafruit_DHT as DHT
 import w1thermsensor as W1
 
-# Require TDDP encryption / decryption integration.
-from .tpl_smart_home_protocol import TPLSmartHomeProtocol
-
 # Define, in seconds, how long between polling intervals.
 SLEEP_INTERVAL = 300
 
@@ -76,8 +73,8 @@ def get_light_state():
     ''' Provides a helper to get the state of the external light (boolean). '''
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((SENSOR_IP_LIGHT, 9999))
-    sock.send(TPLSmartHomeProtocol.encrypt('{"system":{"get_sysinfo":{}}}'))
-    result = json.loads(TPLSmartHomeProtocol.decrypt(sock.recv(2048)[4:]))
+    sock.send(SmartHomeProtocol.encrypt('{"system":{"get_sysinfo":{}}}'))
+    result = json.loads(SmartHomeProtocol.decrypt(sock.recv(2048)[4:]))
 
     # Extract the relevant field from the result and convert to a float. Where
     # 1.0 is 'Light On' and 0.0 is 'Light Off'.
@@ -180,3 +177,47 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# by Lubomir Stroetmann
+# Copyright 2016 softScheck GmbH 
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+class SmartHomeProtocol(object):
+	# Encryption for TP-Link Smart Home Protocol
+	# XOR Autokey Cipher with starting key = 171
+	# https://github.com/softScheck/tplink-smartplug/
+	@staticmethod
+	def encrypt(string):
+		key = 171
+		result = "\0\0\0\0"
+		for i in string: 
+			a = key ^ ord(i)
+			key = a
+			result += chr(a)
+		return result
+
+
+	# Decryption for TP-Link Smart Home Protocol
+	# XOR Autokey Cipher with starting key = 171
+	# https://github.com/softScheck/tplink-smartplug/
+	@staticmethod
+	def decrypt(string):
+		key = 171 
+		result = ""
+		for i in string: 
+			a = key ^ ord(i)
+			key = ord(i) 
+			result += chr(a)
+		return result
