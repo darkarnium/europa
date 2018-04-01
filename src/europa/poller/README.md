2e5baaa2946e44997c91f067683bc95c3907d262
Peter Adkins <peter.adkins@kernelpicnic.net>
2018-04-01 16:16:28 +0100

    Licence for SmartHomeProtocol

--- src/europa/poller/README.md
+++ src/europa/poller/README.md
@@ -119,3 +119,24 @@ research performed by Lubomir Stroetmann and Tobias Esser. The `encryption` and
 from the below website.
 
   * https://www.softscheck.com/en/reverse-engineering-tp-link-hs110/
+
+The `SmartHomeProtocol` class inside of `poller.py` is covered under the
+following licence.
+
+```
+# by Lubomir Stroetmann
+# Copyright 2016 softScheck GmbH 
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+```
--- src/europa/poller/poller.py
+++ src/europa/poller/poller.py
@@ -32,6 +32,50 @@ API_EXTERNAL_LIGHT = 5
 API_BASE_URI = 'http://127.0.0.1:5000/v1'
 
 
+# by Lubomir Stroetmann
+# Copyright 2016 softScheck GmbH 
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+class SmartHomeProtocol(object):
+	# Encryption for TP-Link Smart Home Protocol
+	# XOR Autokey Cipher with starting key = 171
+	# https://github.com/softScheck/tplink-smartplug/
+	@staticmethod
+	def encrypt(string):
+		key = 171
+		result = "\0\0\0\0"
+		for i in string: 
+			a = key ^ ord(i)
+			key = a
+			result += chr(a)
+		return result
+
+
+	# Decryption for TP-Link Smart Home Protocol
+	# XOR Autokey Cipher with starting key = 171
+	# https://github.com/softScheck/tplink-smartplug/
+	@staticmethod
+	def decrypt(string):
+		key = 171 
+		result = ""
+		for i in string: 
+			a = key ^ ord(i)
+			key = ord(i) 
+			result += chr(a)
+		return result
+
+
 def get_ambient_all():
     ''' Provides a helper to get all data from the ambient sensor. '''
     AMBIENT_SENSOR_TYPE = DHT.AM2302
@@ -177,47 +221,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# by Lubomir Stroetmann
-# Copyright 2016 softScheck GmbH 
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#      http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
-class SmartHomeProtocol(object):
-	# Encryption for TP-Link Smart Home Protocol
-	# XOR Autokey Cipher with starting key = 171
-	# https://github.com/softScheck/tplink-smartplug/
-	@staticmethod
-	def encrypt(string):
-		key = 171
-		result = "\0\0\0\0"
-		for i in string: 
-			a = key ^ ord(i)
-			key = a
-			result += chr(a)
-		return result
-
-
-	# Decryption for TP-Link Smart Home Protocol
-	# XOR Autokey Cipher with starting key = 171
-	# https://github.com/softScheck/tplink-smartplug/
-	@staticmethod
-	def decrypt(string):
-		key = 171 
-		result = ""
-		for i in string: 
-			a = key ^ ord(i)
-			key = ord(i) 
-			result += chr(a)
-		return result
